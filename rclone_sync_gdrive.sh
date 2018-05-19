ba3b3b33b825cfa4694a1c03b495b441105c5b2d
Bobses <bobses@gmail.com>
2018-05-19 09:48:49 +0300

    correction on rclone sync with google drive

--- rclone_sync_gdrive.sh
+++ rclone_sync_gdrive.sh
@@ -2,6 +2,8 @@
 #
 RCLONE_CONFIG_PASS=your_rclone_password
 export RCLONE_CONFIG_PASS
+# instead /home/backup put your source directory
+# instead GoogleDrive:backup/server put your GD remote name and your destination directory
 # to exclude all files starting with"."
 rclone sync /home/backup GoogleDrive:backup/server --exclude '.*' --ask-password=false
 # more about rclone filtering here: https://rclone.org/filtering/
